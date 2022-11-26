cf4f43f200ee4c2b41ee5503b92ed7fbb948932c
 Sergey Shalygin <sergeyshalygin@MacBook-Pro-Sergey.local>
2022-11-26 03:11:34 +0300

    sleeping stop

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,7 @@
 import { program } from 'commander';
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.1');
+		.description('Compares two configuration files and shows a difference.')
+		.version('0.1');
 
 program.parse();
