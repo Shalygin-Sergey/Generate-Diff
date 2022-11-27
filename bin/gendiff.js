e0e3989715e6ad8026304becd0b0f408f45c4465
 Sergey Shalygin <sergeyshalygin@MacBook-Pro-Sergey.local>
2022-11-28 01:37:58 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,9 @@ import { program } from 'commander';
 
 program
 		.description('Compares two configuration files and shows a difference.')
-		.version('0.0.1');
+		.version('0.0.1')
+		.option('-f, --format <type>', 'output format')
+		.argument('<filepath1>')
+		.argument('<filepath2>');
 
 program.parse();
