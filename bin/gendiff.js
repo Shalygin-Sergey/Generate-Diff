d95b9400705dacd2dcc18e63b987c5dbdfbaba02
 Sergey Shalygin <sergeyshalygin@MacBook-Pro-Sergey.local>
2022-12-02 02:30:10 +0300

    step 3 build parser

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import parser from "./parser.js";
 
 program
 		.description('Compares two configuration files and shows a difference.')
 		.version('0.0.1')
 		.option('-f, --format <type>', 'output format')
 		.argument('<filepath1>')
-		.argument('<filepath2>');
+		.argument('<filepath2>')
+		.action((firstConfig, secondConfig) => {
+				console.log(parser(firstConfig, secondConfig));
+		});
 
 program.parse();
