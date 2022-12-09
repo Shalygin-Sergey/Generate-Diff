192d24d92a9ae243eaa76450fdb2139941bb3e4b
Шалыгин Сергей <60192169+Shalygin-Sergey@users.noreply.github.com>
2022-12-09 17:37:34 +0300

    Update parser.js

--- bin/parser.js
+++ bin/parser.js
@@ -6,28 +6,28 @@ const diff = (data1, data2) => {
 		const result = [];
 		result.push('{');
 		keys.map((key) => {
-				// если равны ключи и значения
-				if (_.has(data1, key) && _.has(data2, key)) {
-						if (data1[key] === data2[key]) {
-								result.push(`    ${key}: ${data1[key]}`);
-						}
-				}
-				// если равны ключи , но значения разные
-				if (_.has(data1, key) && _.has(data2, key)) {
-						if (data1[key] !== data2[key]) {
-								result.push(`  - ${key}: ${data1[key]}`);
-								result.push(`  + ${key}: ${data2[key]}`);
-						}
-				}
-				// если ключ есть в первом файле но отсутствует во втором
-				if (_.has(data1, key) && !_.has(data2, key)) {
-						result.push(`  - ${key}: ${data1[key]}`);
-				}
+			// если равны ключи и значения
+			if (_.has(data1, key) && _.has(data2, key)) {
+					if (data1[key] === data2[key]) {
+							result.push(`    ${key}: ${data1[key]}`);
+					}
+			}
+			// если равны ключи , но значения разные
+			if (_.has(data1, key) && _.has(data2, key)) {
+					if (data1[key] !== data2[key]) {
+							result.push(`  - ${key}: ${data1[key]}`);
+							result.push(`  + ${key}: ${data2[key]}`);
+					}
+			}
+			// если ключ есть в первом файле но отсутствует во втором
+			if (_.has(data1, key) && !_.has(data2, key)) {
+					result.push(`  - ${key}: ${data1[key]}`);
+			}
 
-				// если ключ есть во втором файле но отсутствует в первом
-				if (!_.has(data1, key) && _.has(data2, key)) {
-						result.push(`  + ${key}: ${data2[key]}`);
-				}
+			// если ключ есть во втором файле но отсутствует в первом
+			if (!_.has(data1, key) && _.has(data2, key)) {
+					result.push(`  + ${key}: ${data2[key]}`);
+			}
 		});
 		result.push('}');
 		return result.join('\n');
