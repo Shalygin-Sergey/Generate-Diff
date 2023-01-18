730286d9afb62216e6d1926d6d55ff48df19bb84
sergeyshalygin <shalygin.javascript@gmail.com>
2023-01-19 01:23:32 +0300

    step 7

--- src/parser.js
+++ src/parser.js
@@ -1,47 +1,27 @@
-import { readFileSync } from 'fs';
 import _ from 'lodash';
 
-const diff = (data1, data2) => {
-	const keys = _.union(_.keys(data1), _.keys(data2)).sort();
-	const getText = keys.flatMap((key) => {
-		const temp = [];
-
-		if (_.has(data1, key) && _.has(data2, key)) {
-			if (data1[key] === data2[key]) {
-				temp.push(`    ${key}: ${data1[key]}`);
-			}
+const buildDiff = (data1, data2) => {
+	const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+	const diff = keys.flatMap((key) => {
+// если ключ есть во втором файле но отсутствует в первом
+		if (!_.has(data1, key)) {
+			return { key, value: data2[key], state: 'added' };
 		}
-		// если равны ключи , но значения разные
-		if (_.has(data1, key) && _.has(data2, key)) {
-			if (data1[key] !== data2[key]) {
-				temp.push(`  - ${key}: ${data1[key]}`);
-				temp.push(`  + ${key}: ${data2[key]}`);
-			}
+// если ключ есть в первом файле но отсутствует во втором
+		if (!_.has(data2, key)) {
+			return { key, value: data1[key], state: 'removed' };
 		}
-		// если ключ есть в первом файле но отсутствует во втором
-		if (_.has(data1, key) && !_.has(data2, key)) {
-			temp.push(`  - ${key}: ${data1[key]}`);
+// если равны ключи и значения
+		if (data1[key] === data2[key]) {
+			return { key, value: data1[key], state: 'unchanged' };
 		}
-
-		// если ключ есть во втором файле но отсутствует в первом
-		if (!_.has(data1, key) && _.has(data2, key)) {
-			temp.push(`  + ${key}: ${data2[key]}`);
+// если есть вложенные объекты
+		if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+			return { key, value: buildDiff(data1[key], data2[key]), state: 'complex' };
 		}
-
-		return temp;
+// если равны ключи , но значения разные
+		return { key, value: { oldValue: data1[key], newValue: data2[key] }, state: 'updated' };
 	});
-	const result = _.concat('{', getText, '}');
-	return _.join(result, '\n');
+	return diff;
 };
-
-const parser = (filepath1, filepath2) => {
-	const data1 = readFileSync(filepath1, 'utf-8');
-	const data2 = readFileSync(filepath2, 'utf-8');
-
-	const dataParse1 = JSON.parse(data1);
-	const dataParse2 = JSON.parse(data2);
-
-	return diff(dataParse1, dataParse2);
-};
-
-export default parser;
+export default buildDiff;
