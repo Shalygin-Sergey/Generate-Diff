b9b9403cfcd497ea8b3f7e9c696d496163ed5428
sergeyshalygin <shalygin.javascript@gmail.com>
2023-01-20 20:55:19 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,10 +3,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import parser from '../src/index.js';
 
-const __fileName = fileURLToPath(import.meta.url);
-const __dirName = path.dirname(__fileName);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const getFixturePath = (fileName) => path.join(__dirName, '..', '__fixtures__', fileName);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('check json file', () => {
 	const fileName1 = getFixturePath('file1.json');
