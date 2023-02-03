8bc8472dcb086954566342c6a496c1c7b8e0db7c
sergeyshalygin <shalygin.javascript@gmail.com>
2023-02-04 01:25:46 +0300

    fixed check#2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -22,24 +22,4 @@ describe('genDiff should work correctly', () => {
     expect(genDiff(filepath1, filepath2, 'json')).toEqual(readFixture('result-json.json'));
   });
 });
-// const extensions = [
-//   ['json', 'json', undefined, 'result-stylish.txt'], ['yaml', 'yaml', undefined, 'result-stylish.txt'],
-//   ['yml', 'yml', undefined, 'result-stylish.txt'], ['json', 'yaml', undefined, 'result-stylish.txt'],
-//
-//   ['json', 'json', 'stylish', 'result-stylish.txt'], ['yaml', 'yaml', 'stylish', 'result-stylish.txt'],
-//   ['yml', 'yml', 'stylish', 'result-stylish.txt'], ['json', 'yaml', 'stylish', 'result-stylish.txt'],
-//
-//   ['json', 'json', 'plain', 'result-plain.txt'], ['yaml', 'yaml', 'plain', 'result-plain.txt'],
-//   ['yml', 'yml', 'plain', 'result-plain.txt'], ['json', 'yaml', 'plain', 'result-plain.txt'],
-//
-//   ['json', 'json', 'json', 'result-json.json'], ['yaml', 'yaml', 'json', 'result-json.json'],
-//   ['yml', 'yml', 'json', 'result-json.json'], ['json', 'yaml', 'json', 'result-json.json'],
-// ];
-//
-// test.each(extensions)('file extensions and format(%s, %s, %s)', (file1Extension, file2Extension, format, resultFile) => {
-//   const fileAfter = getFixturePath(`file1.${file1Extension}`);
-//   const fileBefore = getFixturePath(`file2.${file2Extension}`);
-//   const result = fs.readFileSync(getFixturePath(resultFile), 'utf8');
-//
-//   expect(genDiff(fileAfter, fileBefore, format)).toBe(result);
-// });
+
